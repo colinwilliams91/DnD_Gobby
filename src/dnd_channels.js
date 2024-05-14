419d9011ac44fcf18b657592fbc0e3b52c37d347
Colin Williams <colin.williams.dev@gmail.com>
2024-05-13 22:58:17 -0500

    chore: refactor data obj exports to arrays

--- src/dnd_channels.js
+++ src/dnd_channels.js
@@ -1,9 +1,10 @@
-export const CHANNEL_IDS = {
-    "campaign_i": "559499867047591948",
-    "map_art_screenshots": "1237154023476559972",
-    "lore": "1237163165763113000",
-    "in_character_chat": "1238267954374639627",
-    "general": "1239265493043380238"
-};
+export const CHANNEL_IDS = [
+    "768827884520472597", // "reaction-role-testing":
+    "559499867047591948", // "campaign_i":
+    "1237154023476559972", // "map_art_screenshots":
+    "1237163165763113000", // "lore":
+    "1238267954374639627", // "in_character_chat":
+    "1239265493043380238" // "general": (t)
+];
 
 export default CHANNEL_IDS;

6ec96a696c39dfeef9d4ad267dee7f81f85c9966
Colin Williams <colin.williams.dev@gmail.com>
2024-05-18 21:28:12 -0500

    feat: move fns to handlers, WIP, refactor to interactions API

--- src/handlers.js
+++ src/handlers.js
@@ -1,28 +1,35 @@
 export class EventHandlers {
 
     _emojis;
+    _utils;
 
-    constructor(emojis) {
+    constructor(emojis, utils) {
         this._emojis = emojis;
+        this._utils = utils;
     }
 
-    handleNameInput = async (message, prefix) => {
-
-        if (!message.content.startsWith(prefix) || message.author.bot) {
-            console.error(`calling incorrect handler: handleNameInput for ${message}`);
-            return;
-        }
+    updateUserNickname = async (message, prefix) => {
+        const emoji = this._utils.getRandomEmoji();
 
         // TODO: business logic to change user-nickname..
         const args = message.content.slice(prefix.length).trim().split(" ");
         const command = args.shift().toLowerCase();
 
         if (!args.length) {
-			return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
+			return message.channel.send(`You didn't provide any arguments, ${message.author}!😵`);
 		}
 
         message.channel.send(`Command name: ${command}\nArguments: ${args}`);
-        message.channel.send(`Nickname for user: ${message.author.globalName} updated. `);
+        message.channel.send(`Nickname for user: ${message.author.globalName} updated. ${emoji}`);
 
     }
+
+    logMessage = (message) => {
+        console.log(message);
+    };
+
+    reactToMessage = (message) => {
+        const emoji = this._utils.getRandomEmoji();
+        message.react(emoji);
+    };
 }

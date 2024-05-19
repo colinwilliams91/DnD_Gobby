b28191cced601ddf1b753af9867c6ad3a0a9bf3e
Colin Williams <colin.williams.dev@gmail.com>
2024-05-19 18:52:32 -0500

    chore: SlashCommandBuilder commands array module

--- src/commands.js
+++ src/commands.js
@@ -1,10 +1,11 @@
 import { SlashCommandBuilder } from "discord.js";
 
-export default {
-    data: new SlashCommandBuilder()
-        .setName("ping")
-        .setDescription("Replies with Pong!"),
-    async execute(interaction) {
-        await interaction.reply("Pong!");
-    },
-};
+/**
+ * @abstract array of commands
+ * @generator `SlashCommandBuilder` - A builder that creates API-compatible JSON data for slash commands.
+ * @example old commands API: `{ name: "", description: "", options: {} }`
+ */
+export default [
+    new SlashCommandBuilder().setName("ping").setDescription("Replies with Pong!"),
+    new SlashCommandBuilder().setName("test").setDescription("Replies with test!"),
+];

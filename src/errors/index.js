a01bda402cb08afc65b45cf8dbaa0f8d4e256224
Colin Williams <colin.williams.dev@gmail.com>
2024-05-23 00:06:30 -0500

    chore: bot send errors ENUM

---
+++ src/errors/index.js
@@ -0,0 +1,4 @@
+export const ERRORS = Object.freeze({
+    NO_NAME: "Please use the (/name Character Name) command first, before using (/say) and try again...",
+    NO_AVATAR: "Character name does not have an avatar. Please submit an image to... #channel and please try again."
+});

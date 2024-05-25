f04ce395ac7435c1c74675c14e586c8826aa082e
Colin Williams <colin.williams.dev@gmail.com>
2024-05-25 14:51:00 -0500

    chore: cache clear script and node run

---
+++ scripts/clear-packages.ps1
@@ -0,0 +1,16 @@
+# This will clear the node_modules cached packages and reinstall for Powershell
+# Run from root
+
+# Check if script execution is blocked due to execution policy
+if ((Get-ExecutionPolicy) -eq "Restricted") {
+    Write-Host "Script execution is currently blocked due to the execution policy."
+    Write-Host "We will now bypass the execution policy for this script."
+
+    # Bypass the execution policy for this session and execute the script
+    & powershell.exe -ExecutionPolicy Bypass -File .\scripts\packages-clear.ps1
+  }
+
+  Remove-Item -Recurse -Force .\node_modules\
+  Remove-Item -Force package-lock.json
+
+  npm i

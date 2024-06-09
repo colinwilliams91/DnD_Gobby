af77640e705d9b6ad00e1d6359995a192ad28927
Colin Williams <colin.williams.dev@gmail.com>
2024-06-09 01:20:56 -0500

    feat: add TRDL cloudinary avatar img url to AVATARS map

--- src/data/avatars.js
+++ src/data/avatars.js
@@ -2,5 +2,6 @@ const AVATARS = new Map();
 
 AVATARS.set("Bot", "https://res.cloudinary.com/dbdyc4klu/image/upload/v1716346071/discord_dnd_bot/fb2c340f-5b3d-4a3e-ab36-f26bc1a73cb7_l43xx2.webp");
 AVATARS.set("Stoak", "https://res.cloudinary.com/dbdyc4klu/image/upload/v1716347451/discord_dnd_bot/Stoak_Tinder_Photo_giisf7.webp");
+AVATARS.set("TRDL", "https://res.cloudinary.com/dbdyc4klu/image/upload/v1717904339/discord_dnd_bot/Tortle-5e_ykjt8e.webp");
 
 export default AVATARS;
